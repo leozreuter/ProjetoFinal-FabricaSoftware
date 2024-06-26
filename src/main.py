c2f591edbc907e4cbeea62e2b944cbc7821dea4d
Leonardo Reuter <165732957+leozreuter@users.noreply.github.com>
2024-06-25 23:00:27 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 import os
 import subprocess
+import platform
 
 def run_django_server():
     # Caminho relativo para o diretório do projeto Django
-    project_directory = os.path.join(os.path.dirname(__file__), 'PSF_GESTAO_FINANCEIRA')
+    project_directory = os.path.join(os.path.dirname(__file__), 'src', 'PSF_GESTAO_FINANCEIRA')
     manage_py = os.path.join(project_directory, 'manage.py')
     
     # Navegar para o diretório do projeto
     os.chdir(project_directory)
     
+    # Verificar o sistema operacional
+    if platform.system() == 'Windows':
+        python_executable = 'python'
+    else:
+        python_executable = 'python3'
+    
     # Executar o comando para iniciar o servidor Django
-    subprocess.run(['python', manage_py, 'runserver'])
+    subprocess.run([python_executable, manage_py, 'runserver'])
 
 if __name__ == "__main__":
     run_django_server()
