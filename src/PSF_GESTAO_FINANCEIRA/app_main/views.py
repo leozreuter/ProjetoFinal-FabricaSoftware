fb8ce27107299adb53a204950a2a07f62112f6a6
Leo Reuter - PC <leo.reuter@hotmail.com>
2024-07-06 18:10:17 -0300

    Add front newPlanej and others

--- src/PSF_GESTAO_FINANCEIRA/app_main/views.py
+++ src/PSF_GESTAO_FINANCEIRA/app_main/views.py
@@ -9,20 +9,46 @@ from .planejamentos import Planejamentos
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
-def planejamentos(request):
-    planejamento_obj = Planejamentos(request.user)
-    lista_planejamentos = planejamento_obj.listar_planejamentos()
 
-    for planejamento in lista_planejamentos:
-        planejamento.barra_iterable = range(planejamento.barra)
-
-    return render(request, 'planejamentos/planejamentos.html', {'planejamentos': lista_planejamentos})
 
 def movimentacoes(request):
     return render(request, 'movimentacoes/movimentacoes.html')
 
-@login_required
+
+
+
+
+@login_required # Redirecione para a página de login
+def configuracoes(request):
+    return render(request, 'configuracoes/configuracoes.html')
+
+def perfil(request):
+    if not request.user.is_authenticated:
+        return redirect('login')  # Redirecione para a página de login
+    return render(request, 'home/home.html')
+
+# ----------------------------------------------------------------------------------------------------------------------------- #
+# PLANEJAMENTOS
+
+# LISTAR PLANEJAMENTOS
+@login_required # Redirecione para a página de login
+def planejamentos(request):
+    planejamento_obj = Planejamentos(request.user)
+    lista_planejamentos = planejamento_obj.listar_planejamentos()
+
+    lista_vazia = not lista_planejamentos
+
+    if lista_vazia:
+        print('true')
+
+    for planejamento in lista_planejamentos:
+        planejamento.barra_iterable = range(planejamento.barra)
+
+    return render(request, 'planejamentos/planejamentos.html', {'planejamentos': lista_planejamentos, 'verificaVazio': lista_vazia})
+
+
+# NOVO PLANEJAMENTOS
+@login_required # Redirecione para a página de login
 def newplanejamento(request):
     if request.method == 'POST':
         form = PlanejamentoForm(request.POST)
@@ -35,13 +61,20 @@ def newplanejamento(request):
         form = PlanejamentoForm()
     return render(request, 'planejamentos/novoplanejamento.html', {'form': form})
 
-def configuracoes(request):
-    return render(request, 'configuracoes/configuracoes.html')
 
-def perfil(request):
-    if not request.user.is_authenticated:
-        return redirect('login')  # Redirecione para a página de login
-    return render(request, 'home/home.html')
+# EDIT PLANEJAMENTOS
+@login_required # Redirecione para a página de login
+def editplanej(request):
+    return render(request, 'planejamentos/editplanejamentos.html')
+
+
+# ----------------------------------------------------------------------------------------------------------------------------- #
+# HOME
+def home(request):
+    username = request.session.get('username', 'visitante')
+    return render(request, 'home/home.html',{'username': username})
+
+
 
 def cadastro(request):
     cad = Autenticacao()
@@ -50,21 +83,6 @@ def cadastro(request):
 def login(request):
     cad = Autenticacao()
     return cad.login(request)
-
-# ----------------------------------------------------------------------------------------------------------------------------- #
-# HOME
-
-@login_required
-def editplanej(request):
-    return render(request, 'planejamentos/editplanejamentos.html')
-
-
-
-# ----------------------------------------------------------------------------------------------------------------------------- #
-# HOME
-def home(request):
-    username = request.session.get('username', 'visitante')
-    return render(request, 'home/home.html',{'username': username})
 
 def logout_view(request):
     logout(request)
