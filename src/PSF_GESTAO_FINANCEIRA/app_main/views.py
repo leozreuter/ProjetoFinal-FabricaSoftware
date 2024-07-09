da6d09c2724d76398456745f7919f072e367736b
Rafazanella01 <rafazanella2004@gmail.com>
2024-07-09 19:36:14 -0300

    Documentação views

--- src/PSF_GESTAO_FINANCEIRA/app_main/views.py
+++ src/PSF_GESTAO_FINANCEIRA/app_main/views.py
@@ -1,37 +1,68 @@
-from pyexpat.errors import messages
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,  logout
+from django.contrib.auth import authenticate, logout
 from .autenticacao import Autenticacao
 from .forms import PlanejamentoForm
 from .planejamentos import Planejamentos
 from django.contrib.auth.decorators import login_required
 import random
 
+def movimentacoes(request):
+    """
+    Renderiza a página de movimentações.
 
+    Parametros:
+        request: A solicitação HTTP recebida.
 
-def movimentacoes(request): # VERIFICA SE O USER ESTA LOGADO // Redirecione para a página de login
+    Returns:
+        HttpResponse: A resposta HTTP contendo a página de movimentações.
+    """
     return render(request, 'movimentacoes/movimentacoes.html')
 
-@login_required # VERIFICA SE O USER ESTA LOGADO // Redirecione para a página de login
+@login_required
 def configuracoes(request):
+    """
+    Renderiza a página de configurações. Verifica se o usuário está logado.
+
+    Parametros:
+        request: A solicitação HTTP recebida.
+
+    Returns:
+        HttpResponse: A resposta HTTP contendo a página de configurações.
+    """
     return render(request, 'configuracoes/configuracoes.html')
 
 def perfil(request):
+    """
+    Renderiza a página de perfil. Se o usuário não estiver autenticado, redireciona para a página de login.
+
+    Parametros:
+        request: A solicitação HTTP recebida.
+
+    Returns:
+        HttpResponse: A resposta HTTP contendo a página de perfil ou redirecionamento para a página de login.
+    """
     if not request.user.is_authenticated:
-        return redirect('login')  # VERIFICA SE O USER ESTA LOGADO // Redirecione para a página de login
+        return redirect('login')
     return render(request, 'home/home.html')
 
 # ----------------------------------------------------------------------------------------------------------------------------- #
 # PLANEJAMENTOS
 
-# LISTAR PLANEJAMENTOS
-@login_required # VERIFICA SE O USER ESTA LOGADO // Redirecione para a página de login
+@login_required
 def planejamentos(request):
+    """
+    Lista os planejamentos do usuário logado.
+
+    Parametros:
+        request: A solicitação HTTP recebida.
+
+    Returns:
+        HttpResponse: A resposta HTTP contendo a lista de planejamentos ou uma mensagem indicando que a lista está vazia.
+    """
     planejamento_obj = Planejamentos(request.user)
     lista_planejamentos = planejamento_obj.listar_planejamentos()
-
     lista_vazia = not lista_planejamentos
 
     if lista_vazia:
@@ -42,10 +73,18 @@ def planejamentos(request):
 
     return render(request, 'planejamentos/planejamentos.html', {'planejamentos': lista_planejamentos, 'verificaVazio': lista_vazia})
 
-# NOVO PLANEJAMENTOS
-@login_required # VERIFICA SE O USER ESTA LOGADO // Redirecione para a página de login
+@login_required
 def newplanejamento(request):
-    if request.method == 'POST':  # Verifica se o método que está chamando é postagem de informações
+    """
+    Cria um novo planejamento. Se o método de solicitação for POST, tenta criar um planejamento com os dados do formulário.
+
+    Parametros:
+        request: A solicitação HTTP recebida.
+
+    Returns:
+        HttpResponse: A resposta HTTP contendo o formulário para criar um novo planejamento ou redirecionamento para a lista de planejamentos.
+    """
+    if request.method == 'POST':
         form = PlanejamentoForm(request.POST)
         planejamentos = Planejamentos(request.user)
         if planejamentos.criar_planejamento(form):
@@ -56,9 +95,17 @@ def newplanejamento(request):
         form = PlanejamentoForm()
     return render(request, 'planejamentos/novoplanejamento.html', {'form': form})
 
-# EDIT PLANEJAMENTOS
-@login_required # VERIFICA SE O USER ESTA LOGADO // Redirecione para a página de login
+@login_required
 def editplanej(request):
+    """
+    Edita um planejamento existente. Renderiza a página de edição de planejamento.
+
+    Parametros:
+        request: A solicitação HTTP recebida.
+
+    Returns:
+        HttpResponse: A resposta HTTP contendo a página de edição de planejamento.
+    """
     if request.method == 'POST':
         form = PlanejamentoForm(request.POST)
         planejamentos = Planejamentos(request.user)
@@ -74,8 +121,17 @@ def editplanej(request):
 
 # ----------------------------------------------------------------------------------------------------------------------------- #
 # HOME
+
 def home(request):
-    # Criado um dicionário com as duas chaves (frase e autor), assim conseguimos usar a biblioteca random para selecionar uma frase aleatoria
+    """
+    Renderiza a página inicial com uma frase motivacional aleatória.
+
+    Parametros:
+        request: A solicitação HTTP recebida.
+
+    Returns:
+        HttpResponse: A resposta HTTP contendo a página inicial com uma frase motivacional aleatória.
+    """
     frases = [
         {"frase": "Uma jornada de mil quilômetros precisa começar com um simples passo.", "autor": "Lao Tzu"},
         {"frase": "A riqueza é consequência de trabalho e poupança.", "autor": "Benjamin Franklin"},
@@ -84,17 +140,14 @@ def home(request):
         {"frase": "Dinheiro é apenas uma ferramenta. Ele irá levá-lo onde quiser, mas não vai substituí-lo como motorista.", "autor": "Ayn Rand"},
         {"frase": "Cuidado com as pequenas despesas, um pequeno vazamento afundará um grande navio.", "autor": "Benjamin Franklin"},
         {"frase": "As pessoas gastam um dinheiro que não têm, para comprar coisas de que elas não precisam, para impressionar pessoas de quem não gostam.", "autor": "Will Rogers"},
-        {"frase": "A educação formal vai fazer você ganhar a vida. A autoeducação vai fazer você alcançar uma fortuna..", "autor": "Jim Rohn"},
+        {"frase": "A educação formal vai fazer você ganhar a vida. A autoeducação vai fazer você alcançar uma fortuna.", "autor": "Jim Rohn"},
         {"frase": "O único lugar em que sucesso vem antes de trabalho é no dicionário.", "autor": "Vidal Sassoon"},
-        {"frase": " dinheiro é um mestre terrível, mas um excelente servo.", "autor": "P. T. Barnum"},
+        {"frase": "Dinheiro é um mestre terrível, mas um excelente servo.", "autor": "P. T. Barnum"},
         {"frase": "A maneira mais rápida de ganhar dinheiro é resolver um problema. Quanto maior for o problema a resolver, mais dinheiro que você vai ganhar.", "autor": "Steve Siebold"},
-        
     ]
 
-    # Seleciona uma frase aleatoria
     frase_escolhida = random.choice(frases)
 
-    # Aqui tem outro dicionario que usamos para passar as variaveis pra o HTML
     contexto = {
         "username": request.session.get('username', 'Visitante'),
         "frase": frase_escolhida["frase"],
@@ -103,13 +156,40 @@ def home(request):
     return render(request, 'home/home.html', contexto)
 
 def cadastro(request):
+    """
+    Renderiza a página de cadastro e realiza o processo de cadastro de usuário.
+
+    Parametros:
+        request: A solicitação HTTP recebida.
+
+    Returns:
+        HttpResponse: A resposta HTTP contendo a página de cadastro.
+    """
     cad = Autenticacao()
     return cad.cadastro(request)
 
 def login(request):
+    """
+    Renderiza a página de login e realiza o processo de autenticação do usuário.
+
+    Parametros:
+        request: A solicitação HTTP recebida.
+
+    Returns:
+        HttpResponse: A resposta HTTP contendo a página de login.
+    """
     cad = Autenticacao()
     return cad.login(request)
 
 def logout_view(request):
+    """
+    Realiza o logout do usuário e redireciona para a página de login.
+
+    Parametros:
+        request: A solicitação HTTP recebida.
+
+    Returns:
+        HttpResponse: A resposta HTTP contendo a página de login após o logout.
+    """
     logout(request)
-    return render(request, 'login/login.html')  # Redirecione para a página de login ou outra página
+    return render(request, 'login/login.html')
